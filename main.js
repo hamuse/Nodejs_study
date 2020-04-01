24fdf7fd8b6c7eeb6f6518fbd9191d77992ca887
hamuse <53929403+hamuse@users.noreply.github.com>
2020-04-01 16:32:08 +0900

    express 미들웨어 study1 200401 pm 04:31
    express 미들웨어 study1 200401 pm 04:31

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ const fs = require('fs');
 const template = require('./lib/template.js');
 const path = require('path');
 const sanitizeHtml = require('sanitize-html');
+const qs = require('querystring');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
 
 app.get('/', function (request, response) {
     fs.readdir('./data', function (error, filelist) {
@@ -19,7 +25,146 @@ app.get('/', function (request, response) {
 });
 
 app.get('/page/:pageId', function (request, response) {
-    response.send(request, response);
+    fs.readdir('./data', function (error, filelist) {
+        let filteredId = path.parse(request.params.pageId).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+            let title = request.params.pageId;
+            let sanitizedTitle = sanitizeHtml(title);
+            let sanitizedDescription = sanitizeHtml(description, {
+                allowedTags:['h1']
+            });
+            let list = template.list(filelist);
+            let html = template.html(title, list,
+                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+                `<a href="/create">create</a>
+                                    <a href="/update/${sanitizedTitle}">update</a>
+                                   <form action="/delete_process" method="post" onsubmit="deleteis">
+                                   <input type="hidden" name="id" value="${sanitizedTitle}">
+                                   <input type="submit" value="delete">
+                                   </form>`);
+            response.send(html);
+        });
+    });
+});
+
+app.get('/create',function (request, response) {
+    fs.readdir('./data', function (error, filelist) {
+        console.log(filelist);
+        let title = 'WEB - create';
+        let list = template.list(filelist);
+        let html = template.html(title, list, `
+                <form action="/create_process" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                    <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+                </form>`, '');
+        response.send(html);
+    });
+});
+
+app.post('/create_process',function (request, response) {
+    /*let body = '';
+    request.on('data', function (data) {
+        body = body + data;
+
+    });
+    request.on('end', function () {
+        let post = qs.parse(body);
+        let title = post.title;
+        let description = post.description;
+        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
+        })
+    });*/
+
+    let post = request.body;
+    let title = post.title;
+    let description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+    })
+});
+
+app.get('/update/:pageId',function (request, response) {
+    fs.readdir('./data', function (error, filelist) {
+        let filteredId = path.parse(request.params.pageId).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+            var title = request.params.pageId;
+            let list = template.list(filelist);
+            var html = template.html(title, list,
+                `
+                     <form action="/update_process" method="post">
+                        <input type="hidden" name="id" value="${title}">
+                        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                        <p>
+                            <textarea name="description" placeholder="description">${description}</textarea>
+                        </p>
+                        <p>
+                            <input type="submit">
+                        </p>
+                     </form>`,
+                `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+            response.send(html);
+        });
+    });
+});
+
+app.post('/update_process',function (request, response) {
+   /* let body = '';
+    request.on('data', function (data) {
+        body = body + data;
+    });
+    request.on('end', function () {
+        let post = qs.parse(body);
+        let id = post.id;
+        let title = post.title;
+        let description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function (error) {
+            fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+           /!*     response.writeHead(302, {Location: `/?id=${title}`});
+                response.end();*!/
+           response.redirect(`/?id=${title}`);
+            });
+        });
+    });*/
+    let post = request.body;
+    let id = post.id;
+    let title = post.title;
+    let description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function (error) {
+        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+           response.redirect(`/?id=${title}`);
+            });
+        });
+});
+
+app.post('/delete_process', function (request, response) {
+   /* let body = '';
+    request.on('data', function (data) {
+        body = body + data;
+    });
+    request.on('end', function () {
+        let post = qs.parse(body);
+        let id = post.id;
+        let filteredId = path.parse(id).base;
+        fs.unlink(`data/${filteredId}`, function (error) {
+           /!* response.writeHead(302, {Location: `/`});
+            response.end();*!/
+           response.redirect('/');
+        })
+    });*/
+    let post = request.body;
+    let id = post.id;
+    let filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function (error) {
+           response.redirect('/');
+        })
 });
 
 app.listen(3001, function () {
